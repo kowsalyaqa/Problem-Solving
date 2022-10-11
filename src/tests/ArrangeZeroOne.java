026aa007a33101d4649dad18aa3df245ad237e39
kowsalya_Karuppusamy <kowsalyaqait22@gmail.com>
2022-10-12 03:49:05 +0530

    ArrangeZerroOne commit

---
+++ src/tests/ArrangeZeroOne.java
@@ -0,0 +1,20 @@
+package tests;
+
+import java.util.Arrays;
+
+public class ArrangeZeroOne {
+
+	public static void main(String[] args) {
+		int[] n= {0,1,1,0,1};
+		if(n.length!=0) {
+			int[] n2=new int[n.length];
+			int start=0,end=n.length-1;
+			for(int i=0;i<n.length;i++) {
+				if(n[i]==0) n2[start++]=n[i];
+				else n2[end--]=n[i];
+			}
+			System.out.println(Arrays.toString(n2));
+		}
+	}
+
+}
